50c4f9b0f351d43cea757029e26e9163f20646dd
aa0965 <anshupratapsingh32391@gmail.com>
2020-04-21 02:17:41 +0530

    'server-build'

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app =express()
+const ppath = path.join(__dirname, '..', 'public')
+app.use(express.static(ppath))
+app.get('*',(req,res) => {
+  res.sendFile(path.join(ppath,'index.html'))
+})
+const port = process.env.PORT||3000
+app.listen(port,()=>{
+  console.log('server is up')
+})
